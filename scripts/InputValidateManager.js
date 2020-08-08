b8ef62dfa0e5144c12d4a636bf8bb76dcc9b1345
MaDRaGe <locodi111@mail.ru>
2020-08-08 20:54:33 +0300

    Added comments in InputValidateManager

--- scripts/InputValidateManager.js
+++ scripts/InputValidateManager.js
@@ -1,16 +1,24 @@
+/*
+  InputValidateManager
+
+  @description - makes result of value validation
+*/
 const InputValidateManager = (function () {
   let instance;
   function init() {
     return {
-      validate: function (settings, value) {
+      /*
+        Validate input value using following rule
+      */
+      validate: function (rule, value) {
         switch (
-          settings.name // Name of validation
+          rule.name // Name of validation
         ) {
           case "regexp": // Regular expression
-            return settings.regexp.test(value);
+            return rule.regexp.test(value);
             break;
           case "equal": // Compare 2 values for equality
-            return value === settings.compareInput.value && value;
+            return value === rule.compareInput.value && value;
             break;
           default:
             console.log("Invalid rule of validation!");
